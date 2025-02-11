715f40297b3dfd83198b212b6ab54b93473ad2d2
KarisAya <1048827424@qq.com>
2025-02-11 12:08:52 +0800

    不把deepseek的思考过程接入上下文

--- clovers_AIchat/ai/deepseek.py
+++ clovers_AIchat/ai/deepseek.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from clovers.logger import logger
 from .openai import Chat as OpenAIChat, ChatCompletionMessageParam
 
 
@@ -6,22 +8,35 @@ class Chat(OpenAIChat):
 
     async def chat(self, nickname: str, text: str, image_url: str | None) -> str | None:
         if text.endswith("--think"):
-            self.think = True
+            think = True
             text = text[:-7].strip()
         else:
-            self.think = False
-        return await super().chat(nickname, text, image_url)
+            think = False
+        now = datetime.now()
+        timestamp = now.timestamp()
+        try:
+            contect = await self.build_content(f'{nickname} ({now.strftime("%Y-%m-%d %H:%M")}):{text}', image_url)
+        except Exception as err:
+            logger.exception(err)
+            return
+        self.messages.append({"time": timestamp, "role": "user", "content": contect})
+        self.memory_filter(timestamp)
+        try:
+            messages: list[ChatCompletionMessageParam] = [{"role": "system", "content": self.prompt_system}]
+            messages.extend({"role": message["role"], "content": message["content"]} for message in self.messages)
+            resp = await self.async_client.chat.completions.create(model=self.model, messages=messages)
+            resp_content = resp.choices[0].message.content
+            self.messages.append({"time": timestamp, "role": "assistant", "content": resp_content})
+            if think:
+                reasoning_content: str | None = getattr(resp.choices[0].message, "reasoning_content", None)
+                if reasoning_content:
+                    return f"<think>\n{reasoning_content}\n</think>\n{resp_content}"
+            return resp_content
+        except Exception as err:
+            del self.messages[-1]
+            logger.exception(err)
+            return
 
     @staticmethod
     async def build_content(text: str, image_url: str | None):
         return text
-
-    async def ChatCompletions(self):
-        messages: list[ChatCompletionMessageParam] = [{"role": "system", "content": self.prompt_system}]
-        messages.extend({"role": message["role"], "content": message["content"]} for message in self.messages)
-        resp = await self.async_client.chat.completions.create(model=self.model, messages=messages)
-        if self.think:
-            reasoning_content: str | None = getattr(resp.choices[0].message, "reasoning_content", None)
-            if reasoning_content:
-                return f"<think>\n{reasoning_content}\n</think>\n{resp.choices[0].message.content}"
-        return resp.choices[0].message.content
